e08d4921992955bd013fd47fc32af492e6b5b726
Kappicht.Jannik <Kappicht.Jannik@DESKTOP-F25JDAB.2631.skool.net>
2018-10-04 08:01:01 +0200

    Gluecksspiel

---
+++ src/Gluecksspiel.java
@@ -0,0 +1,14 @@
+
+public class Gluecksspiel {
+
+	public static void main(String[] args) {
+		
+		if(Math.random()<=0.42){
+			System.out.println("Verloren!");
+		}
+		else{
+			System.out.println("Gewonnen!");
+	}
+
+}
+}
